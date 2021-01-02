a7f88ea42e3d88076455d694928ad6b51fc87b7e
jval1972 <jvalavanis@gmail.com>
2021-01-02 14:17:07 +0200

    Updated year in copyright notice

--- tx_glutils.pas
+++ tx_glutils.pas
@@ -1,7 +1,7 @@
 //------------------------------------------------------------------------------
 //
 //  DD_TEXTURE: A tool for creating textures from real world photos.
-//  Copyright (C) 2017 by Jim Valavanis
+//  Copyright (C) 2017-2021 by Jim Valavanis
 //
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License
